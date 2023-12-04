57f43bc5627e83394b9b885830065e4c759532ab
pedrohenriqueduarte <ph09duarte@gmail.com>
2023-12-04 01:42:50 -0300

    feat: create user module

--- chat-backend/src/app.module.ts
+++ chat-backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, UserModule],
   controllers: [],
   providers: [],
 })
