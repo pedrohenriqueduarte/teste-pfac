484baecf910cc52781ba9233371d456ee235b329
pedrohenriqueduarte <ph09duarte@gmail.com>
2023-12-04 04:24:43 -0300

    fix: add apitags swagger

--- chat-backend/src/message/message.controller.ts
+++ chat-backend/src/message/message.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { MessageService } from './message.service';
 import { CreateMessageDto } from './dto/create-message.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Messages')
 @Controller('message')
 export class MessageController {
   constructor(private readonly messageService: MessageService) {}
