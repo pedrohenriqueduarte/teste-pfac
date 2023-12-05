7b5b70ea28a4a8d9a09ad5a432caceeb531b4055
pedrohenriqueduarte <ph09duarte@gmail.com>
2023-12-04 23:58:41 -0300

    fix: enable cors

--- chat-backend/src/main.ts
+++ chat-backend/src/main.ts
@@ -21,6 +21,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+  });
+
   app.enableShutdownHooks();
   await app.listen(port);
 }
