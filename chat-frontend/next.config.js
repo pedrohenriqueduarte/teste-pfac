47e2963bff559df305df65bd3cf10c8d54d755e8
pedrohenriqueduarte <ph09duarte@gmail.com>
2023-12-06 17:45:50 -0300

    fix: config next

--- chat-frontend/next.config.js
+++ chat-frontend/next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                    { key: 'Access-Control-Allow-Origin', value: 'https://teste-pfac-pedro-henrique-s-projects.vercel.app/' },
+                    { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE' },
+                    { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+                ],
+            },
+        ];
+    },
+}
 
 module.exports = nextConfig
